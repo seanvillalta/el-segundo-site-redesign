d7b8283087c3c86b77965fae2d03360001a4eafd
Sean Villalta <seanvillalta@gmail.com>
2016-06-18 06:37:01 -0700

    Add content, layout and image to visitor page

--- wp-content/themes/es-wp-foundation/template-visitor.php
+++ wp-content/themes/es-wp-foundation/template-visitor.php
@@ -1,52 +1,37 @@
 <?php
 /*
-  Template Name: Template-Visitor
+Template Name: Template-Visitor
  */
 ?>
 
-<?php get_header(); ?>
+<?php get_header();?>
 <br>
-        <div class="row">
-            <div class="medium-6 columns">
-                <img class="thumbnail" src="<?php bloginfo('template_url'); ?>/assets/dest/img/pink-sky.jpg">
-            </div>
-            <div class="medium-6 columns">
-                <h3>Visitor</h3>
-                <p>Visitor: Available Documents, Frequently Asked Questions, and El Segundo Payment Links</p>
-            </div>
-        </div>
-        <hr>
 
         <div class="row">
-          <!-- Main Blog Content -->
+
           <div class="large-9 columns" role="content">
 
             <div class="row">
               <div class="small-12 columns">
-                
-                <h3 class="subheader">News</h3>
-                
+
+                <h3 class="subheader">Visitor</h3>
+
                 <article class="post">
                   <a href="#">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/dest/img/high-school.jpg" />
+                    <img src="<?php bloginfo('template_url');?>/assets/dest/img/high-school.jpg" />
                   </a>
                   <div class="content">
-                    <h2><a href="#">El Segundo High School Shuts Down</a></h2>
-                    <p>Written by <strong>Jacob, Jay and Sean</strong> <span class="verified"></span></p>
+                  <br>
                     <p>Whoever said that no one is perfect obviously never saw Chuck Norris If you ask Chuck Norris what time it is, he always says, "Two seconds 'til." After you ask, "Two seconds 'til what?" he roundhouse kicks you in the face</p>
-                    <p>___</p>
-                    <p>June 11, 2016</p>
                   </div>
-                </article> 
-                
+                </article>
+
               </div>
             </div>
-        
-            <hr />
 
           </div>
           <!-- End Main Content -->
-        
+
           <!-- Sidebar -->
           <br>
           <br>
@@ -58,19 +43,124 @@
               <li><a href="#">Services & Agencies</a></li>
               <li><a href="#">Schools</a></li>
               <li><a href="#">Places of Worship</a></li>
-              <li><a href="#">Clubs & Orginization Meetings</a></li>
+              <li><a href="#">Clubs & Orginizations</a></li>
               <li><a href="#">Hotels</a></li>
-              <li><a href="#">Area Atrractions</a></li>
             </ul>
-        
-            <div class="panel">
-              <h5>Featured</h5>
-              <p>Pork drumstick turkey fugiat. Tri-tip elit turducken pork chop in. Swine short ribs meatball irure bacon nulla pork belly cupidatat meatloaf cow.</p>
-              <a href="#">Read More &rarr;</a>
-            </div>
+
           </aside>
           <!-- End Sidebar -->
         </div>
         <!-- End Main Content and Sidebar -->
+        <div class="row">
+        	<article class="website-example row wide">
+        	    <div class="large-6 columns">
+        	      <h3>Visiting El Segundo</h3>
+        	      <h6>Welcome</h6>
+        	      <p><strong>Would you believe that this city, which is now known as a major aeronautical and industrial business center, home to the Los Angeles Air Force Base and more than 17,000 people, was once a "round clump of willows"?</strong></p>
+        	      <br>
+        	      <blockquote class="cite">
+        	            <p>Prior to El Segundo's incorporation in 1917, this area was part of "Rancho Sausal Redondo" ("Ranch of the Round Clump of Willows"), a rancho with a land mass of nearly 25,000 acres which extended from the areas as far west of what is now Playa del Rey, as far east as Inglewood, and as far south as Redondo Beach. The land consisted of wheat and barley fields on which cattle and sheep grazed.</p>
+        	      </blockquote>
+        	      <p><strong>In May 1911, five men representing the Standard Oil Company arrived here: Richard J. Hanna and J.E. Howell of the Eclipse refinery of Franklin, Pennsylvania and John Black, Henry Foster and William Rheem from the Standard Oil refinery in Point Richmond, a city 18 miles east of San Francisco). They were surveying the area as a potential site for their next oil refinery. What was required was an area adjacent to the seashore so their tankers could have appropriate access. The undeveloped nature of the site appealed to them because land costs had to be kept to a minimum. Also, the site had to be close to populated areas so it could attract enough employees.</strong></p>
+        	      <p>The "clump of willows" was just what Mr. Hanna's team was looking for.</p>
+        	    </div>
+        	    <div class="large-6 columns">
+        	    <h3 style="visibility: hidden;">Council Meeting</h3>
+        	    <h6 style="visibility: hidden;">Built with Foundation</h6>
+        	      <p><strong>Lastly, this new site needed a name. Richard Hanna's wife, Virginia, deemed this expanse as "El Segundo", (Spanish for "the second one,") because the site was to be Standard Oil's second oil refinery in California (The Point Richmond refinery was already christened as "El Primero"). Sometime later, a group of proud but unknown citizens had nicknamed it "El Segundo a nada" (Spanish for 'second to none').</strong></p>
+        	      <br>
+        	      <blockquote class="cite">
+        	            <p>Standard Oil bought 840 acres of this land on June 11. The refinery opened for business, just five and a half months later, on November 27.</p>
+        	       </blockquote>
+        	      <p><strong>On January 18, 1917, less than six years after the opening of the refinery, the City of El Segundo was incorporated.</strong></p>
+        	      <p><a class="button" href="http://www.elsegundo.org/civica/filebank/blobdload.asp?BlobID=7553">More about us »</a></p>
+        	    </div>
+        	  </article>
 
-    <?php get_footer(); ?>
+        	  <article class="website-example row wide">
+        	          	    <div class="large-6 columns">
+        	          	      <h3>Census Data</h3>
+        	          	      <h6>Information is derived from 2010 US Census</h6>
+        	          	      <p><strong>Quick Facts</strong></p>
+        	          	      <p>Total Population: 16,654 <br>Households: 7,281 <br>Houshold Income (median): $89,926 (2009) <br>High School Graduate or higher: 96.2% <br>Bachelor's Degree or higher: 49.0%</p>
+        	          	      <p><strong>Housing</strong></p>
+        	          	      <p>Multi-Family Housholds: 57.2% <br>Single-Family Detached Households: 42.8% <br>Owner-Occupied Housing: 40.9% <br>Tenant-Occupied Housing: 54.7% <br>Vacant Housing: 4.4% <br>Mediian Value of Owner Occupied Housing: $804,800
+        	          	      </p>
+        	          	    </div>
+        	          	    <div class="large-6 columns">
+							<h3 style="visibility: hidden;">Council Meeting</h3>
+							<h6 style="visibility: hidden;">Built with Foundation</h6>
+        	          	    <p><strong>Ethnicity</strong></p>
+        	          	    <p>African American: 300 (1.9%) <br>Caucasian: 11,282 (69.9%) <br>Latin/Hispanic: 2,891 (17.9%) <br>Native American: 116 (0.7%) <br>Native Hawaiian: 31 (0.2%) <br>Other race: 71 (0.4%)</p>
+        	          	    <p><strong>School Enrollment</strong></p>
+        	          	    <p>K-12, El Segundo Unified, 2012-2013 Academic Year: 3,441</p>
+        	          	    </div>
+        	    </article>
+
+        	  <article class="website-example row wide">
+        	      <div class="large-6 columns">
+        	        <h3>Services & Agencies</h3>
+        	        <h6>City of El Segundo</h6>
+        	        <p><strong><a href="http://www.elsegundousd.com/cs/">California Governor</a></strong></p>
+        	        <p><strong>Governor Edmund G. Brown Jr.</strong> <br>State Capitol, Suite 1173 <br>Sacramento, CA 95814 <br>Phone: 916-445-2841 <br>Fax: 916-558-3160</p>
+        	        <p><strong><a href="http://www.elsegundousd.com/cs/">California State Assembly</a></strong></p>
+        	        <p><strong>Assemblymember Autumn Burke</strong> <br>Boulevard, Suite 601 <br>Inglewood, CA 90301 <br>Phone: 916-445-2841 <br>Fax: 916-558-3160</p>
+        	      </div>
+        	      <div class="large-6 columns">
+        	      <h3 style="visibility: hidden;">Council Meeting</h3>
+        	      <h6 style="visibility: hidden;">Built with Foundation</h6>
+        	      <p><strong><a href="http://www.tedlieu.org/contact">U.S. House of Representatives</a></strong></p>
+        	        <p><strong>Representative Ted Lieu</strong> <br>1600 Beach Office, 4th Floor<br>Manhattan Beach, CA 90266 <br>Phone: 916-445-2841 <br>Fax: 916-558-3160</p>
+        	        <p><strong><a href="http://boxer.senate.gov/">United States Senate</a></strong></p>
+        	        <p><strong>U.S. Senator for California Barbara Boxer </strong> <br>312 N. Spring St. Suite 1748<br>Los Angeles, CA 90012<br>Phone: 916-445-2841 <br>Fax: 916-558-3160</p>
+        	        <a class="button" href="#">Click for More Agencies »</a>
+        	      </div>
+        	    </article>
+
+        	  <article class="website-example row wide">
+        	      <div class="large-6 columns">
+        	        <h3>Clubs & Organizations</h3>
+        	        <h6>Locations</h6>
+        	        <p><a href="/civicax/filebank/blobdload.aspx?BlobID=14280"><strong>2016 Waste Hauler List</strong></a><br>
+
+<a href="/depts/works/solid_waste/business_recycling.asp" shape="rect"><strong>Commercial Trash &amp; Recycling</strong></a><br>
+
+<a href="/civicax/filebank/blobdload.aspx?BlobID=10549"><strong>City-Provided Solid Waste &amp; Recycling Services (Single-Family Residences) Brochure</strong></a><br>
+
+<a href="/depts/works/solid_waste/greenwaste_recycling.asp" shape="rect"><strong>Green Waste Recycling</strong></a><br>
+
+<a href="/depts/works/solid_waste/holiday_tree_recycling.asp" shape="rect"><strong>Holiday Tree Recycling</strong></a><br>
+
+<a href="/depts/works/refuse_and_recycling_services/household_hazardous_waste_collection.asp" shape="rect"><strong>Household Hazardous Waste Disposal</strong></a><br>
+
+<a href="/depts/works/solid_waste/large_item_pick_up.asp"><strong>Fall Clean-Up Week 2015: October 12, 13, 14, 15</strong></a><br>
+
+<a href="/depts/works/solid_waste/multi_family_refuse_and_recycling_service.asp" shape="rect"><strong>Multi-Family Solid Waste &amp; Recycling</strong></a><br>
+
+<a href="/depts/works/solid_waste/reducing_is_easy.asp" shape="rect"><strong>Reducing is Easy</strong></a><br>
+
+<a href="/depts/works/solid_waste/recycling_bins.asp" shape="rect"><strong>Recycling Bins</strong></a></p><br>
+        	        <a class="button" href="#">Click for More Info »</a>
+        	      </div>
+        	      <div class="large-6 columns">
+        	      <h3 style="visibility: hidden;">Council Meeting</h3>
+        	      <h6 style="visibility: hidden;">Built with Foundation</h6>
+        	      </div>
+        	    </article>
+
+        	  <article class="website-example row wide">
+        	    <div class="large-6 columns">
+        	      <h3>Hotels</h3>
+        	      <h6>Information</h6>
+        	      <p><strong>Download Airbnb</strong></p>
+        	      <p><i>Please find below the link to the download the app. Really.</i></p>
+        	      <a class="button" href="https://www.airbnb.com">Download »</a>
+        	    </div>
+        	    <div class="large-6 columns">
+        	      <h3 style="visibility: hidden;">Council Meeting</h3>
+        	      <h6 style="visibility: hidden;">Built with Foundation</h6>
+        	    </div>
+        	  </article>
+        </div>
+
+    <?php get_footer();?>
