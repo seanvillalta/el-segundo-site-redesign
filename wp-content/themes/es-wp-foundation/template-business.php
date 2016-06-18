08f6577e52f6a28d97252052e1b4105e55305da3
Sean Villalta <seanvillalta@gmail.com>
2016-06-18 10:52:14 -0700

    Fin

---
+++ wp-content/themes/es-wp-foundation/template-business.php
@@ -0,0 +1,315 @@
+<?php
+/*
+Template Name: Template-Business
+ */
+?>
+
+<?php get_header();?>
+<br>
+
+<div class="row">
+	<!-- Main Blog Content -->
+	<div class="large-9 columns" role="content">
+
+		<div class="row">
+			<div class="small-12 columns">
+
+				<h3 class="subheader">Business</h3>
+
+				<article class="post">
+					<a href="#">
+						<img src="<?php bloginfo('template_url');?>/assets/dest/img/apt-view.jpg" />
+					</a>
+					<div class="content">
+						<br>
+						<p><strong>Chuck Norris can win a game of Connect Four in only three moves. Chuck Norris will attain statehood in 2009. His state flower will be the Magnolia When Chuck Norris searches for a Wii online, he types in "we"... </strong></p>
+					</div>
+				</article>
+
+			</div>
+		</div>
+
+	</div>
+	<!-- End Main Content -->
+
+	<!-- Sidebar -->
+	<br>
+	<br>
+	<aside class="large-3 columns">
+		<h5>Categories</h5>
+		<ul class="no-bullet">
+			<li><a href="#">Business Services</a></li>
+			<li><a href="#">Climate</a></li>
+			<li><a href="#">History</a></li>
+			<li><a href="#">Transportation Services</a></li>
+			<li><a href="#">Schools</a></li>
+			<li><a href="#">Travel</a></li>
+			<li><a href="#">Available Documents</a></li>
+		</ul>
+
+	</aside>
+	<!-- End Sidebar -->
+</div>
+<!-- End Main Content and Sidebar -->
+<div class="row">
+	<article class="website-example row wide">
+		<div class="large-6 columns">
+			<h3>Open Your Business Today</h3>
+			<h6>Come and Voice Your Opinions</h6>
+			<p>The City Council meets the first and third Tuesday of the month at 7 pm. in the City Hall Council Chamber, 350 Main Street. For more information call the City Council office at 524-2302.</p>
+			<br>
+			<blockquote class="cite">
+				<p>Unless otherwise noted in the Agenda, the Public can only comment on City-related business that is within the jurisdiction of the City Council and/or items listed on the Agenda during the <strong>Public Communications</strong> portions of the Meeting. Additionally, the Public can comment on any Public Hearing item on the Agenda during the Public Hearing portion of such item. The time limit for comments is five (5) minutes per person.</p>
+			</blockquote>
+			<p><i>Before speaking to the City Council, please come to the podium and state: Your name and residence and the organization you represent, if desired. Please respect the time limits.</i></p>
+		</div>
+		<div class="large-6 columns">
+			<h3 style="visibility: hidden;">Council Meeting</h3>
+			<h6 style="visibility: hidden;">Built with Foundation</h6>
+			<p>Members of the Public may place items on the second Public Communications portion of the Agenda by submitting a <strong>Written Request</strong> to the City Clerk or City Manager's Office at least six days prior to the City Council Meeting (by 2:00 p.m. the prior Tuesday). The request must include a brief general description of the business to be transacted or discussed at the meeting. Playing of video tapes or use of visual aids may be permitted during meetings if they are submitted to the City Clerk two (2) working days prior to the meeting and they do not exceed five (5) minutes in length.</p>
+			<p>In compliance with the Americans with Disabilities Act, if you need special assistance to participate in this meeting, please contact the City Clerk at 310-524-2307. Notification 48 hours prior to the meeting will enable the City to make reasonable arrangements to ensure accessibility to this meeting.</p>
+			<a class="button" href="#">Take a Closer Look »</a>
+		</div>
+	</article>
+
+	<article class="website-example row wide">
+		<div class="large-6 columns">
+			<h3>History</h3>
+			<h6>FAQ's</h6>
+			<p><strong>Q. How do I get a Library Card?</strong></p>
+			<p><i>El Segundo, now home to a population of more than 16,000 people and a number of Fortune 500 Corporate offices, was mostly uninhabited land used for dry farming.</i></p>
+			<p><strong>The land was once part of the Sausal Redondo Rancho controlled by Daniel Freeman, the founder of Inglewood. In 1885, most of the rancho was sold in square mile sections to wealthy investors. While the beach areas of the south attracted some residents, the El Segundo and airport land in late 1910 was vacant. Standard Oil Company, recognizing the need for a refinery in Southern California, sent a large party of executives from the East as well as California to scout for a site in November 1910.</strong></p>
+			<p><i>In late May 1911, construction had begun on the refinery after a 2-mile long rail spur serving the site was completed. One of the first major tasks was dealing with the sand dunes. About 400 mules, mule-skinners, and Fresno scrapers were brought in to level sites for the refinery structures and storage tanks. In September 1911, Hanna invited Rheem down from San Francisco to "fire" the No. 1 battery of stills starting the production of end products. Raw crude had been accumulating in storage tanks having arrived by pipeline from the Whittier-Fullerton oil fields. By the end of the year, reportedly Standard Oil had spent over $1,000,000 of the $3,000,000 allotted to complete the refinery.</i></p>
+						<p><i>A. Yes, except for videos or overdue items, you can renew by phone: (310) 524-2722.</i></p>
+			<a class="button" href="#">Click on this link for more Library information »</a>
+		</div>
+		<div class="large-6 columns">
+		<h3 style="visibility: hidden;">Council Meeting</h3>
+		<h6 style="visibility: hidden;">Built with Foundation</h6>
+		<p>1917	- January 18, the City incorporates.</p>
+		<p>1919	- March 10, there is a major fire on Richmond Street.  The Library moved to City Hall. </p>
+		<p>1920	- General Chemical Company (followed by Allied, Honeywell and now El Segundo Plaza) builds a plant on Sepulveda in El Segundo. </p>
+		<p>1923	- Standard Oil Company spends $10-15 million on equipment program, making plant one of the largest of its kind in the world; Police and Water Departments established. </p>
+		<p>1928	- January 3, Converse Drugstore opens. El Segundo High School opens with an enrollment of 124. </p>
+		<p>1929	- August 28, Graf Zeppelin lands at Mines Field.  Moreland Aircraft Plant opens. El Segundo High Shool graduates first class of 37 students. </p>
+		<p>1930	- Pickwick Nitecoach opens. El Segundo Library opens with 1400 books.</p>
+		<p>1933	- Earthquake damages plunge, city hall and school buildings; the Converse Building becomes second home for Library.</p>
+		<p>1935	- General Tile Company begins operations.</p>
+
+		<br>
+
+		</div>
+	</article>
+
+	<article class="website-example row wide">
+		<div class="large-6 columns">
+			<h3>Travel</h3>
+			<h6>Water, Power & Cable</h6>
+			<p><strong>Edison International (Southern California Edison)</strong></p>
+			<p>Customer Service (Residential) - (800) 655-4555 <br>Customer Service (Business) - (800) 990-7788 <br>Account Balances/Billing Questions - (800) 950-2356</p>
+			<p><strong>The Gas Company</strong></p>
+			<p>Customer Service (Residential) - (800) 427-2200 <br>Customer Service (Business) - (800) 427-2000 <br>Excavation Pipeline Locations - (800) 227-2600</p>
+			<p><strong>Direct TV El Segundo</strong></p>
+			<p>Customer Service (Residential) - (800) 877-7746 <br>Customer Service (Business) - (800) 877-2000 <br>Repair - (800) 222-3000</p>
+			<a class="button" href="#">Click for More Info »</a>
+		</div>
+		<div class="large-6 columns">
+			<h3>Climate</h3>
+			<h6>Built with Foundation</h6>
+			<p><strong>To Report a Street Light Out to Southern California Edison directly:</strong></p>
+			<p><a href="https://www.sce.com/Onc/StreetLightOutage.aspx"><strong>Click on This Link</font></strong>.</a></p>
+		</div>
+	</article>
+
+	<article class="website-example row wide">
+		<div class="large-6 columns">
+			<h3>Finance Director</h3>
+			<h6>Joseph Lillio</h6>
+			<p><strong><a href="http://www.elsegundousd.com/cs/">Finance Director </a></strong></p>
+			<p><strong><a href="http://elsegundousd.com/richmond_school/default.html">310-524-2315</a></strong></p>
+			<p>jlillo@elsegundo.org</p>
+
+			<h3>Administrative Specialist</h3>
+			<h6>Shannon Rowan</h6>
+			<p><strong><a href="http://www.elsegundousd.com/cs/">Finance Director </a></strong></p>
+			<p><strong><a href="http://elsegundousd.com/richmond_school/default.html">310-524-2315</a></strong></p>
+			<p>srowan@elsegundo.org</p>
+		</div>
+		<div class="large-6 columns">
+			<h3 style="visibility: hidden;">Council Meeting</h3>
+			<h6 style="visibility: hidden;">Built with Foundation</h6>
+		</div>
+	</article>
+
+	<article class="website-example row wide">
+		<div class="large-6 columns">
+			<h3>Travel</h3>
+			<h6>Locations</h6>
+			<p>El Segundo may be a small community, but there's no shortage of ways to get here. In fact, few places can offer a location as well suited for the go-anywhere demands of business and industry. In a mere five-square-mile radius, area businesses have access to two major freeways, a light rail station, a bus depot and LAX. We're still working on the shuttle launch pad.</p>
+			<p>A hop, skip and a jump from practically everywhere. El Segundo is a backyard community. Of course, that backyard does include LAX. Which means you're never too far away from where you want to be.</p>
+			<br>
+
+			<table width="100%" align="">
+			    <tbody>
+			        <tr>
+			            <th align="left" colspan="2">DESTINATION TIMETABLE</th>
+			        </tr>
+			        <tr>
+			            <td>San Diego</td>
+			            <td>30 minutes</td>
+			        </tr>
+			        <tr>
+			            <td>San Francisco</td>
+			            <td>45 minutes</td>
+			        </tr>
+			        <tr>
+			            <td>New York</td>
+			            <td>5 hours</td>
+			        </tr>
+			        <tr>
+			            <td>Chicago</td>
+			            <td>3 hours, 53 minutes</td>
+			        </tr>
+			        <tr>
+			            <td>Boston</td>
+			            <td>5 hours, 16 minutes</td>
+			        </tr>
+			    </tbody>
+			</table>
+			<p>Call the Metropolitan Transit Authority (MTA) at 213-626-4455 for a bus schedule.</p>
+			</div>
+			<div class="large-6 columns">
+				<h3 style="visibility: hidden;">Council Meeting</h3>
+				<h6 style="visibility: hidden;">Built with Foundation</h6>
+				<table width="60%" align="center">
+				    <tbody>
+				        <tr>
+				            <th align="left" colspan="2">EL SEGUNDO-AREA TRANSPORTATION</th>
+				        </tr>
+				        <tr>
+				            <td valign="top">Airplane</td>
+				            <td valign="top">Los Angeles International Airport</td>
+				        </tr>
+				        <tr>
+				            <td valign="top">Automobile</td>
+				            <td valign="top">San Diego Freeway (I-405)<br>
+				            Glenn Anderson Freeway (I-105)<br>
+				            State Highway #1</td>
+				        </tr>
+				        <tr>
+				            <td valign="top">Bus</td>
+				            <td valign="top">
+				                <a href="/depts/recreation/brochure_and_programs/shuttle_services.asp"><strong>Lunchtime Shuttle</strong></a>
+				                <a href="http://www.mta.net/"><strong>MTA</strong></a>
+				                <a href="http://www.redondo.org/depts/hbt/transit/default.asp"><strong>BCT</strong></a>
+				            </td>
+				        </tr>
+				        <tr>
+				            <td valign="top">Light Rail</td>
+				            <td valign="top">Metro Green Line<a href="http://www.mta.net/"></a></td>
+				        </tr>
+				        <tr>
+				            <td></td>
+				            <td><a href="/civica/filebank/blobdload.asp?BlobID=4597"></a></td>
+				        </tr>
+				        <tr>
+				            <td valign="top">Trucks</td>
+				            <td valign="top">All major truck lines</td>
+				        </tr>
+				        <tr>
+				            <td valign="top">Water</td>
+				            <td valign="top">Marina Del Rey Harbor<br>
+				            King Harbor at Redondo Beach<br>
+				            Port of Los Angeles at San Pedro</td>
+				        </tr>
+				    </tbody>
+				</table>
+			</div>
+		</article>
+
+		<article class="website-example row wide">
+			<div class="large-12 columns">
+				<h3 class="text-center">City of El Segundo Transportation Services</h3>
+				    <p align="center">
+				        <strong>City of El Segundo
+				    Transportation Services<br></strong>Schedule a ride on El Segundo Dial-A-Ride by contacting Dispatch at (310) 524-2704 or send an email to <a href="mailto:dialaride@elsegundo.org?subject=Dial%20A%20Ride%20Appointment">dialaride@elsegundo.org</a>.<br><br><br>
+				    <table align="center">
+				        <tbody>
+				            <tr>
+				                <td>                   
+				                	<p align="center"><img alt="" height="109" src="http://www.elsegundo.org/images/transportationlogo_1.bmp" style="height: 109px; width: 150px" width="150"></p>
+				                </td>
+				                <td>
+				                    <p align="center"><strong>Dial a Ride Dispatch hours:<br></strong> Monday-Friday 8:00am-5:00pm<br>
+				                    Saturday 9:00am-1:00pm<br>
+				                    Sunday 11:00am-4:00pm</p>
+				                </td>
+				            </tr>
+				            <tr>
+				                <td>
+				                    <p align="center"><a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?BlobID=13633">Dial a Ride Schedule</a></p>
+				                </td>
+				                <td>
+				                    <p align="center"><strong>Dial a Ride Service Hours:<br></strong> Monday-Friday<br>
+				                    8:30am-3:00pm</p>
+				                </td>
+				            </tr>
+				            <tr>
+				                <td>
+				                    <p align="center"><a href="http://www.elsegundo.org//civicax/filebank/blobdload.aspx?BlobID=12249" target="_blank">Dr. Dial a Ride Service Area</a></p>
+				                </td>
+				                <td>
+				                    <p align="center"><strong>Dr. Dial a Ride Medical
+				                    Appointment Service Hours</strong>:<br>
+				                    Thursdays 8:00am-2:00pm</p>
+				                </td>
+				            </tr>
+				            <tr>
+				                <td>
+				                    <p align="center"><a href="http://www.elsegundo.org//civicax/filebank/blobdload.aspx?BlobID=12250" target="_blank">Lunchtime Shuttle Route Brochure</a></p>
+				                </td>
+				                <td>
+				                    <p align="center"><strong>Lunchtime Shuttle
+				                    Service Hours:<br></strong> Monday-Friday<br>
+				                    11:15am-2:00pm</p>
+				                </td>
+				            </tr>
+				        </tbody>
+				    </table><br><br>
+				    <br>
+				    <br>
+				    <div class="text-center">
+				    	<strong>Local Transportation Resources</strong>
+				    </div>
+				    <br>
+				    <div align="center">
+				        <table>
+				            <tbody>
+				                <tr>
+				                    <td>
+				                        <a href="http://www.metro.net/" target=
+				                        "_blank"><img alt="" border="1" src=
+				                        "http://www.elsegundo.org/images/MetroLogo.jpg" style=
+				                        "height: 56px;border: 0px; width: 133px;"
+				                        usemap="#rade_img_map_NewsEditor_1"></a>
+				                    </td>
+				                    <td>Metro Transit Authority</td>
+				                </tr>
+				                <tr>
+				                    <td>
+				                        <a href="http://www.redondo.org/depts/recreation/transit/beach_cities_transit/default.asp" target="_blank"><img alt="" border="1" src="http://www.elsegundo.org//images/BCTLogo.jpg" style="height: 65px; width: 110px; border: 0px;" usemap="#rade_img_map_NewsEditor_0"></a>
+				                    </td>
+				                    <td>Beach Cities Transit</td>
+				                </tr>
+				            </tbody>
+				        </table>
+				    </div>
+			</div>
+			<!-- <div class="large-6 columns">
+				<h3 style="visibility: hidden;">Council Meeting</h3>
+				<h6 style="visibility: hidden;">Built with Foundation</h6>
+			</div> -->
+		</article>
+	</div>
+
+	<?php get_footer();?>
