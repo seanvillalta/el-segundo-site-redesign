a23c16d445fbfc153f6af41058edf93e20c2911f
Sean Villalta <seanvillalta@gmail.com>
2016-06-18 04:50:11 -0700

    Create template for resident page

---
+++ wp-content/themes/es-wp-foundation/template-resident.php
@@ -0,0 +1,177 @@
+<?php
+/*
+Template Name: Template-Resident
+ */
+?>
+
+<?php get_header();?>
+<br>
+
+        <div class="row">
+          <!-- Main Blog Content -->
+          <div class="large-9 columns" role="content">
+
+            <div class="row">
+              <div class="small-12 columns">
+
+                <h3 class="subheader">Resident</h3>
+
+                <article class="post">
+                  <a href="#">
+                    <img src="<?php bloginfo('template_url');?>/assets/dest/img/high-school.jpg" />
+                  </a>
+                  <div class="content">
+                  <br>
+                    <p>Whoever said that no one is perfect obviously never saw Chuck Norris If you ask Chuck Norris what time it is, he always says, "Two seconds 'til." After you ask, "Two seconds 'til what?" he roundhouse kicks you in the face</p>
+                  </div>
+                </article>
+
+              </div>
+            </div>
+
+          </div>
+          <!-- End Main Content -->
+
+          <!-- Sidebar -->
+          <br>
+          <br>
+          <aside class="large-3 columns">
+            <h5>Categories</h5>
+            <ul class="no-bullet">
+              <li><a href="#">Council Meeting</a></li>
+              <li><a href="#">Getting A Library</a></li>
+              <li><a href="#">Street Light Outage</a></li>
+              <li><a href="#">Utilities</a></li>
+              <li><a href="#">Schools</a></li>
+              <li><a href="#">Solid Waste & Recycling</a></li>
+              <li><a href="#">Pets</a></li>
+              <li><a href="#">Express</a></li>
+            </ul>
+
+          </aside>
+          <!-- End Sidebar -->
+        </div>
+        <!-- End Main Content and Sidebar -->
+        <div class="row">
+        	<article class="website-example row wide">
+        	    <div class="large-6 columns">
+        	      <h3>Council Meeting</h3>
+        	      <h6>built with foundation</h6>
+        	      <p>The City Council meets the first and third Tuesday of the month at 7 pm. in the City Hall Council Chamber, 350 Main Street. For more information call the City Council office at 524-2302.</p>
+        	      <p>Unless otherwise noted in the Agenda, the Public can only comment on City-related business that is within the jurisdiction of the City Council and/or items listed on the Agenda during the <strong>Public Communications</strong> portions of the Meeting. Additionally, the Public can comment on any Public Hearing item on the Agenda during the Public Hearing portion of such item. The time limit for comments is five (5) minutes per person.</p>
+        	      <p><i>Before speaking to the City Council, please come to the podium and state: Your name and residence and the organization you represent, if desired. Please respect the time limits.</i></p>
+        	    </div>
+        	    <div class="large-6 columns">
+        	    <h3 style="visibility: hidden;">Council Meeting</h3>
+        	    <h6 style="visibility: hidden;">Built with Foundation</h6>
+        	      <p>Members of the Public may place items on the second Public Communications portion of the Agenda by submitting a <strong>Written Request</strong> to the City Clerk or City Manager's Office at least six days prior to the City Council Meeting (by 2:00 p.m. the prior Tuesday). The request must include a brief general description of the business to be transacted or discussed at the meeting. Playing of video tapes or use of visual aids may be permitted during meetings if they are submitted to the City Clerk two (2) working days prior to the meeting and they do not exceed five (5) minutes in length.</p>
+        	      <p>In compliance with the Americans with Disabilities Act, if you need special assistance to participate in this meeting, please contact the City Clerk at 310-524-2307. Notification 48 hours prior to the meeting will enable the City to make reasonable arrangements to ensure accessibility to this meeting.</p>
+        	      <a class="button" href="#">Take a Closer Look »</a>
+        	    </div>
+        	  </article>
+
+        	  <article class="website-example row wide">
+        	          	    <div class="large-6 columns">
+        	          	      <h3>Getting a Library Card</h3>
+        	          	      <h6>FAQ's</h6>
+        	          	      <p><strong>Q. How do I get a Library Card?</strong></p>
+        	          	      <p><i>A. You need to bring a current driver's license, utility bill or other identification with your current address to the Circulation Desk to verify your address, and then fill out a Library Card Application. Cards are free for local residents and for those residing in a <a href="http://www.socallibraries.org">Southern California Library Cooperative (SCLC)</a> city; others are charged an annual fee of $40.</i></p>
+        	          	      <p><strong>Q. Does the Library Have DVDs, Videos or Books-on-Tape?</strong></p>
+        	          	      <p><i>A. Yes. The Library has over 6,000 videos, DVDs and over 1,000 Books-on-Tape.</i></p>
+        	          	      <p><strong>Q. What About <strong>Borrowing, Returning &amp; Renewing Materials</strong>?</strong></p>
+        	          	      <p><i>A. Most materials are loaned for a period of three weeks. Videos and DVD's are loaned for three days with a $1.50 per video charge. Video proceeds go back into an account to purchase new videos. For more circulation rules and guidelines, inquire at the Circulation Desk, (310) 524-2722.</i></p>
+        	          	      <p><strong>Q. Can I Renew Materials over the Phone?</strong></p>
+        	          	      <p><i>A. Yes, except for videos or overdue items, you can renew by phone: (310) 524-2722.</i></p>
+        	          	      <a class="button" href="#">Click on this link for more Library information »</a>
+        	          	    </div>
+        	          	    <div class="large-6 columns">
+        	          	    </div>
+        	    </article>
+
+        	  <article class="website-example row wide">
+        	      <div class="large-6 columns">
+        	        <h3>Utilities</h3>
+        	        <h6>Water, Power & Cable</h6>
+        	        <p><strong>Edison International (Southern California Edison)</strong></p>
+        	        <p>Customer Service (Residential) - (800) 655-4555 <br>Customer Service (Business) - (800) 990-7788 <br>Account Balances/Billing Questions - (800) 950-2356</p>
+        	        <p><strong>The Gas Company</strong></p>
+        	        <p>Customer Service (Residential) - (800) 427-2200 <br>Customer Service (Business) - (800) 427-2000 <br>Excavation Pipeline Locations - (800) 227-2600</p>
+        	        <p><strong>Direct TV El Segundo</strong></p>
+        	        <p>Customer Service (Residential) - (800) 877-7746 <br>Customer Service (Business) - (800) 877-2000 <br>Repair - (800) 222-3000</p>
+        	        <a class="button" href="#">Click for More Info »</a>
+        	      </div>
+        	      <div class="large-6 columns">
+        	      <h3>Street Light Outage</h3>
+        	      <h6>Built with Foundation</h6>
+        	      <p><strong>To Report a Street Light Out to Southern California Edison directly:</strong></p>
+        	      <p><a href="https://www.sce.com/Onc/StreetLightOutage.aspx"><strong>Click on This Link</font></strong>.</a></p>
+        	      </div>
+        	    </article>
+
+        	  <article class="website-example row wide">
+        	      <div class="large-6 columns">
+        	        <h3>El Segundo Unified School District</h3>
+        	        <h6>Public Schools</h6>
+        	        <p><strong><a href="http://www.elsegundousd.com/cs/">Center Street Elementary School</a></strong></p>
+        	        <p>641 Sheldon Street <br>(310) 615-2650, ext. 225 <br>Geoff Yantz, Superintendent</p>
+        	        <p><strong><a href="http://elsegundousd.com/richmond_school/default.html">Richmond Street Elementary School</a></strong></p>
+        	        <p>615 Richmond Street <br>(310) 606-6831 <br>Dickie Van Breene, Principal</p>
+        	        <p><strong><a href="http://www.elsegundousd.com/esms/index.html">El Segundo Middle School</a></strong></p>
+        	        <p>332 Center Street <br>(310) 615-2690 <br>Dave Lubs, Principal</p>
+        	        <p><strong><a href="http://www.elsegundousd.com/eshs/">El Segundo High School</a></strong></p>
+        	        <p>640 Main Street, El Segundo <br>Jim Garza, Principal (310) 615-2662 <br>Attendance Office (310) 615-2665</p>
+        	        <a class="button" href="#">Click for More Info »</a>
+        	      </div>
+        	      <div class="large-6 columns">
+        	      <h3 style="visibility: hidden;">Council Meeting</h3>
+        	      <h6 style="visibility: hidden;">Built with Foundation</h6>
+        	      </div>
+        	    </article>
+
+        	  <article class="website-example row wide">
+        	      <div class="large-6 columns">
+        	        <h3>Solid Waste & Recycling</h3>
+        	        <h6>Locations</h6>
+        	        <p><a href="/civicax/filebank/blobdload.aspx?BlobID=14280"><strong>2016 Waste Hauler List</strong></a><br>
+
+<a href="/depts/works/solid_waste/business_recycling.asp" shape="rect"><strong>Commercial Trash &amp; Recycling</strong></a><br>
+
+<a href="/civicax/filebank/blobdload.aspx?BlobID=10549"><strong>City-Provided Solid Waste &amp; Recycling Services (Single-Family Residences) Brochure</strong></a><br>
+
+<a href="/depts/works/solid_waste/greenwaste_recycling.asp" shape="rect"><strong>Green Waste Recycling</strong></a><br>
+
+<a href="/depts/works/solid_waste/holiday_tree_recycling.asp" shape="rect"><strong>Holiday Tree Recycling</strong></a><br>
+
+<a href="/depts/works/refuse_and_recycling_services/household_hazardous_waste_collection.asp" shape="rect"><strong>Household Hazardous Waste Disposal</strong></a><br>
+
+<a href="/depts/works/solid_waste/large_item_pick_up.asp"><strong>Fall Clean-Up Week 2015: October 12, 13, 14, 15</strong></a><br>
+
+<a href="/depts/works/solid_waste/multi_family_refuse_and_recycling_service.asp" shape="rect"><strong>Multi-Family Solid Waste &amp; Recycling</strong></a><br>
+
+<a href="/depts/works/solid_waste/reducing_is_easy.asp" shape="rect"><strong>Reducing is Easy</strong></a><br>
+
+<a href="/depts/works/solid_waste/recycling_bins.asp" shape="rect"><strong>Recycling Bins</strong></a></p><br>
+        	        <a class="button" href="#">Click for More Info »</a>
+        	      </div>
+        	      <div class="large-6 columns">
+        	      <h3 style="visibility: hidden;">Council Meeting</h3>
+        	      <h6 style="visibility: hidden;">Built with Foundation</h6>
+        	      </div>
+        	    </article>
+
+        	  <article class="website-example row wide">
+        	    <div class="large-6 columns">
+        	      <h3>Pets</h3>
+        	      <h6>Information</h6>
+        	      <p><strong>All dogs, ages four months and older, must be licensed in the City of El Segundo.</strong></p>
+        	      <p><i>Please find below the link to the online dog license application. You can either bring this form with you to the police station or you can mail it in along with the proper licensing fee to the El Segundo Police Department, attn: Animal Control Division, 348 Main Street, El Segundo, California 90245</i></p>
+        	      <a class="button" href="/depts/pdffiles/petlicense.pdf">Pet License Application »</a>
+        	    </div>
+        	    <div class="large-6 columns">
+        	      <h3 style="visibility: hidden;">Council Meeting</h3>
+        	      <h6 style="visibility: hidden;">Built with Foundation</h6>
+        	    </div>
+        	  </article>
+        </div>
+
+    <?php get_footer();?>
